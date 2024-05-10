f9a636ea433aa947334770d45adddcfff0cd0982
Prashant Singh <singhprashant0314@gmail.com>
2024-05-11 02:40:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello from the server api!");
+});
+
+//post data into DB (CREATE)
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//to get data from DB (READ-1)
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//to fetch a specific data from DB  (READ-2)
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://prashantsingh:ocQmBTHDn4PSS7Ou@backenddb.cupgd2n.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("connected to database!");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("connection to database failed...");
+  });
